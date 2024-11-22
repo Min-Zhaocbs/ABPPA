7a3c1122dd815f0a2fee84a61f879955aee4f928
Min-Zhaocbs <min.zhao@cbs-mail.de>
2024-11-22 17:46:07 +0100

    Add files via upload

---
+++ ABPPA/work.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for, request, render_template, jsonify
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
